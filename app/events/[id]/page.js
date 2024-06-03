06e3ef26ec0bc8b3d9cbc1cf32b01d51b8b99968
M-henriksen <mathias.r.henriksen@gmail.com>
2024-06-03 14:28:22 +0200

    gjort en del endringer på events

--- app/events/[id]/page.js
+++ app/events/[id]/page.js
@@ -11,32 +11,74 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { db } from '@/lib/firebase'
+import { onSnapshot, doc, collection } from 'firebase/firestore'
 
 export default function Events({ params }) {
     //params.id
+    const docId = params.id
+    const docRef = doc(db, "events", docId)
+    const postsCollectionRef = collection(db, "events", docId, "posts")
+
+    const [event, setEvent] = useState({})
+    const [posts, setPosts] = useState([])
+    const [attendanceStatus, setAttendanceStatus] = useState("")
+
+    useEffect(() => {
+        const unsubGroup = onSnapshot(docRef, (docSnapshot) => {
+            console.log(docSnapshot);
+            if (docSnapshot.exists()) {
+                setEvent(docSnapshot.data());
+            }
+        });
+
+        //må ha en fuksjon som henter attendence statusen på bruker
+
+        //q parameter og sort på tid
+        const unsubPosts = onSnapshot(postsCollectionRef, (snapshot) => {
+            const data = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data() };
+            });
+            setPosts(data);
+        });
+
+        return () => {
+            unsubGroup();
+            unsubPosts()
+        };
+    }, []);
+
+
+    const handleAttendanceStatus = async () => {
+
+    }
+
     return (
         <div className='flex flex-col p-10 gap-5'>
             <div className='relative w-full h-[300px]'>
                 <img src="https://miro.medium.com/v2/resize:fit:1400/1*ydhn1QPAKsrbt6UWfn3YnA.jpeg" alt="img" className='w-full h-full object-cover' />
+                <div className='absolute inset-0 flex  top-12 left-12'>
+                    <span className=' text-white text-3xl font-bold'>{event.title}</span>
+                </div>
             </div>
             <div className='flex flex-col gap-4'>
                 <div className='flex justify-between'>
                     <div className='flex flex-col gap-2'>
                         <Label className="text-xl font-bold">dato og tid</Label>
-                        <span>29.05.2023 at 15:30</span>
+                        <span>{event.date}</span>
                     </div>
 
                     <div className='flex flex-col gap-2'>
                         <Label className="text-xl font-bold">Lokasjon</Label>
-                        <span>Bergen works</span>
+                        <span>{event.location}</span>
                     </div>
 
                     <div className='flex flex-col gap-2 text-center'>
                         <Label className="text-xl font-bold">Attendance</Label>
                         <div className='flex gap-3'>
-                            <Button variant="outline">Yes</Button>
-                            <Button variant="outline">No</Button>
-                            <Button variant="outline">Maybe</Button>
+                            <Button variant={attendanceStatus === "yes" ? "" : "outline"} onClick={() => { handleAttendanceStatus("yes"), setAttendanceStatus("yes") }}>Ja</Button>
+                            <Button variant={attendanceStatus === "no" ? "" : "outline"} onClick={() => { handleAttendanceStatus("no"), setAttendanceStatus("no") }}>Nei</Button>
+                            <Button variant={attendanceStatus === "maybe" ? "" : "outline"} onClick={() => { handleAttendanceStatus("maybe"), setAttendanceStatus("maybe") }}>Kanskje</Button>
                         </div>
                     </div>
                 </div>
--- app/events/page.js
+++ app/events/page.js
@@ -16,31 +16,61 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
+    DialogFooter,
 } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectGroup,
+    SelectLabel
+} from "@/components/ui/select"
 import { Button } from '@/components/ui/button'
 import { Badge } from "@/components/ui/badge"
 import { db } from '@/lib/firebase'
-import { collection, onSnapshot, where, query } from 'firebase/firestore'
+import { collection, onSnapshot, where, query, getDocs } from 'firebase/firestore'
 //import Image from 'next/image'
 import Image from "@/components/background.jpg"
 import { auth } from '@/lib/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
+import { Textarea } from '@/components/ui/textarea'
+import { createNewEvent } from '@/lib/events'
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import dayjs from "dayjs"
 
 export default function Events() {
     const router = useRouter()
     const eventsCollectionRef = collection(db, "events")
     const [user, setUser] = useState("")
+    const [users, setUsers] = useState([])
     const [events, setEvents] = useState([])
-
+    const [userToAdd, setUserToAdd] = useState({})
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [date, setDate] = useState()
+    const [image, setImage] = useState(null)
+    const [location, setLocation] = useState("")
+    const [addingMember, setAddingMember] = useState([])
 
     const [categories, setCategories] = useState([
-        { id: 1, title: "alle", value: false, qValue: user.uid, qParam: "creator" },
-        { id: 2, title: "Gruppe 2", value: false },
-        { id: 3, title: "Gruppe 3", value: false },
-        { id: 4, title: "Gruppe 4", value: false },
+        { id: 1, title: "Alle", value: false, qValue: user.uid, qParam: "creator" },
+        { id: 2, title: "Skal på", value: false },
+        { id: 3, title: "Kanskje skal", value: false },
+        { id: 4, title: "Innen en uke", value: false },
     ])
-    const [qParam, setQParam] = useState("")
+    const [qParam, setQParam] = useState({ qValue: "creator", param: "SY9pmFtGFyXocwcMPMIvsmAUsAh1" })
 
     const handleToggle = (id, title) => {
         setCategories(categories.map(category =>
@@ -48,10 +78,10 @@ export default function Events() {
                 ? { ...category, value: true }
                 : { ...category, value: false }
         ));
-
         setQParam({ qValue: user.uid, param: "creator" });
     };
 
+    console.log(qParam)
     useEffect(() => {
         const unsubUser = onAuthStateChanged(auth, (currentUser) => {
             if (currentUser) {
@@ -60,10 +90,14 @@ export default function Events() {
                 router.push("/login")
             }
         });
-        //lag heller en switch på riktig kategori
+
+
+
+        //param som mine arrangmeneter, skal på. kanskje skal på. innen en uke. og i dag
 
         const evnetsCollectionRef = collection(db, "events")
-        const q = query(evnetsCollectionRef, where(qParam.qValue === qParam.param))
+        const q = query(evnetsCollectionRef, where("creator", "==", "7Xb3FhzT1UUdP5BeB84GNwQfnQo1"))
+        console.log("viser querry", q)
         const unsubscribeEvents = onSnapshot(q, (snapshot) => {
             const data = snapshot.docs.map((doc) => {
                 return { id: doc.id, ...doc.data() };
@@ -79,13 +113,42 @@ export default function Events() {
 
     console.log("Event data:", events)
 
-    const handleRedirect = () => {
-
+    const handleRedirect = (id) => {
+        router.push(`/events/${id}`)
     }
+
+    const handleGetUsers = async () => {
+        console.log("did run get user");
+        const usersRef = collection(db, "users");
+        try {
+            const userData = await getDocs(usersRef);
+            const users = userData.docs.map(doc => doc.data());
+            setUsers(users);
+        } catch (error) {
+            console.error("Error getting documents: ", error);
+        }
+    }
+
+    const handleCreatingNewGroup = async () => {
+        try {
+            const res = await createNewEvent(title, description, date, location, image, addingMember, user.uid,)
+            console.log(res)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const addingNewMembers = () => {
+        const fullName = userToAdd.firstName + " " + userToAdd.lastName
+        setAddingMember([...addingMember, { added: dayjs().format('YYYY-MM-DD'), fullName: fullName, uid: userToAdd.uid }])
+    }
+
+    console.log("adding members", addingMember)
 
     return (
         <div className='flex flex-col p-10 gap-5'>
-            <div className='relative w-[80vw] h-[300px]'>
+            <div className='relative w-[80vw] h-[350px]'>
                 <img src="https://miro.medium.com/v2/resize:fit:1400/1*ydhn1QPAKsrbt6UWfn3YnA.jpeg" alt="img" className='w-full h-full object-cover' />
                 <div className='absolute inset-0 flex  top-12 left-12'>
                     <span className=' text-white text-3xl font-bold'>Se alle nye eventer</span>
@@ -99,41 +162,94 @@ export default function Events() {
                     </Badge>
                 ))}
                 <Dialog>
-                    <DialogTrigger className='ml-5'>Lag nytt arrangement</DialogTrigger>
+                    <DialogTrigger className='ml-5' onClick={handleGetUsers}>Lag nytt arrangement</DialogTrigger>
                     <DialogContent>
                         <DialogHeader>
-                            <DialogTitle>Are you absolutely sure?</DialogTitle>
+                            <DialogTitle>Legg til nytt arrangement</DialogTitle>
                             <DialogDescription>
-                                This action cannot be undone. This will permanently delete your account
-                                and remove your data from our servers.
+                                Her kan du opprette ett nytt arrangement, velg ett navn en beskrivelse. Sett en dato inviter brukere
                             </DialogDescription>
                         </DialogHeader>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="title">Tittel</Label>
+                                <Input id="title" placeholder="Event navn" onChange={(e) => setTitle(e.target.value)} />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="pic">Bilde</Label>
+                                <Input id="pic" type="file" onChange={(e) => setImage(e.target.files[0])} />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="description">Beskrivelse</Label>
+                                <Textarea id="description" placeholder="Beskrivelse" onChange={(e) => setDescription(e.target.value)} />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="datetime">Dato</Label>
+                                <Input type="datetime-local" onChange={(e) => setDate(e.target.value)} />
+                            </div>
+
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="datetime">Lokasjon</Label>
+                                <Input type="datetime-local" onChange={(e) => setLocation(e.target.value)} />
+                            </div>
+
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="date">Medlem som er lagt til</Label>
+                                {addingMember.map((member, index) => (
+                                    <span key={index}>{member.fullName}</span>
+                                ))}
+                            </div>
+                            <div className='flex gap-3 w-full'>
+                                <Select onValueChange={(value) => setUserToAdd(value)} >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Velg en bruker" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            {
+                                                users.length === 0
+                                                    ? <SelectLabel>Laster brukere ... </SelectLabel>
+                                                    : <SelectLabel>Brukere</SelectLabel>
+                                            }
+                                            {users.map((user) => (
+                                                <SelectItem key={user.uid} value={user}>{user.firstName} {user.lastname}</SelectItem>
+                                            ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                                <Button onClick={addingNewMembers}>Legg til medlem</Button>
+                            </div>
+                        </div>
+                        <DialogFooter className="w-full mt-5">
+                            <Button type="submit" className="w-full" onClick={handleCreatingNewGroup}>Opprett event</Button>
+                        </DialogFooter>
                     </DialogContent>
                 </Dialog>
             </div>
             <div>
-                <Card className="w-fit">
-                    <CardHeader>
-                        <CardTitle>Gruppe møte</CardTitle>
-                        <CardDescription>Join us for a fun group meetup at the local cafe</CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex flex-row gap-3">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHKEBak-4eVMbinGqXuaH_TJKFQuePXrpixg&s" alt="" className=' w-40 h-28 object-cover' />
-                        <div className='flex flex-col gap-2'>
-                            <Label className="text-l font-semibold">Dato: 24 Jun at 16:30</Label>
-                            <Label className="text-l font-semibold">Medlemmer:</Label>
-                            <div className='flex flex-col'>
-                                <span>eht</span>
-                                <span>eht</span>
-                                <span>eht</span>
-                                <span>eht</span>
+                {events.map((event, index) => (
+                    <Card className="w-fit" key={index}>
+                        <CardHeader>
+                            <CardTitle>{event.title}</CardTitle>
+                            <CardDescription>{event.description}</CardDescription>
+                        </CardHeader>
+                        <CardContent className="flex flex-row gap-3">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHKEBak-4eVMbinGqXuaH_TJKFQuePXrpixg&s" alt="" className=' w-40 h-28 object-cover' />
+                            <div className='flex flex-col gap-2'>
+                                <Label className="text-l font-semibold">Dato: {event.date}</Label>
+                                <Label className="text-l font-semibold">Medlemmer:</Label>
+                                <div className='flex flex-col'>
+                                    {event.memberInfo.slice(0, 5).map((member, index) => (
+                                        <span key={index}>{member.fullName}</span>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    </CardContent>
-                    <CardFooter className="flex justify-end">
-                        <Button variant="ghost" onClick={handleRedirect()}>Les mer</Button>
-                    </CardFooter>
-                </Card>
+                        </CardContent>
+                        <CardFooter className="flex justify-end">
+                            <Button variant="ghost" onClick={() => handleRedirect(event.id)}>Les mer</Button>
+                        </CardFooter>
+                    </Card>
+                ))}
             </div>
         </div>
     )
