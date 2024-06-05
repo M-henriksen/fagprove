5ac0adfc056dae7dcbf294add566c94427222441
M-henriksen <mathias.r.henriksen@gmail.com>
2024-06-05 14:47:15 +0200

    glemt å ta vekk resett passord

--- app/(loginscreens)/login/page.js
+++ app/(loginscreens)/login/page.js
@@ -16,7 +16,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import Link from "next/link"
-import { resetPassword, signIn } from "@/lib/auth";
+import { signIn } from "@/lib/auth";
 
 export default function Login() {
   const router = useRouter()
